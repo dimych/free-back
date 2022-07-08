0ec3971beb6bea08d1897e3315f84074b41f7777
Dimych <kuzyuberdin@gmail.com>
2022-07-08 21:19:56 +0300

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
 // app, use middleware that parse request body
 app.use(express.json())
-
+app.use(cors())
+app.use()
 
 
 let courses = [
